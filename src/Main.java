b48bdc5576c67f1425af31cac6f697263fc88c89
Eyup Yilmaz <eyupy99@gmail.com>
2022-09-28 11:28:21 +0200

    Aufgabe1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello");
+import java.util.Random;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Erstelle eine Zufallszahl zwischen 0 und 100
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+            System.out.println(randomNumber);
+
+        if(randomNumber<20) {
+            System.out.println("Mini");
+        }
+
+        if((randomNumber>=20) && (randomNumber<=50)){
+            System.out.println("Medium");
+    }
+        if(randomNumber>50) {
+            System.out.println("Large");
+        }
     }
 }
