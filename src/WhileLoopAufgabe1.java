888a6ae7a2e01bdc777e4a92de802fdcdd20d91d
Eyup Yilmaz <eyupy99@gmail.com>
2022-10-03 11:41:50 +0200

    commit

---
+++ src/WhileLoopAufgabe1.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class WhileLoopAufgabe1 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        boolean isFinished = false;
+
+        while (!isFinished) {
+            int randomNumber = random.nextInt(10, 31);
+            System.out.println(randomNumber);
+
+            if () {
+
+            }
+         }
+    }
+}
