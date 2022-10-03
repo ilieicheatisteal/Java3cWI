bec0a453f4ccbadc8c551e4804be132aed8113fb
Eyup Yilmaz <eyupy99@gmail.com>
2022-10-03 12:07:13 +0200

    switch

---
+++ src/switchaufgabe.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class switchaufgabe {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(5, 11);
+        System.out.println(randomNumber);
+
+        switch(randomNumber){
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            case 10:
+                System.out.println("Ten");
+                break;
+        }
+    }
+}
