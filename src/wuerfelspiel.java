9c0689fc29ac918b072f3362b94e0611dfe97850
Eyup Yilmaz <eyupy99@gmail.com>
2022-10-03 19:47:47 +0200

    würfel verbesserung

--- src/wuerfelspiel.java
+++ src/wuerfelspiel.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class wuerfelspiel {
     public static void main(String[] args) {
         boolean isFinished = false;
-        int wuerfe = 6;
+        int wuerfe = 5;
         System.out.println("WÜRFELSPIEL");
         System.out.println("    [1] Spiel Starten");
         System.out.println("    [2]Spielregeln");
@@ -12,14 +12,17 @@ public class wuerfelspiel {
         while (!isFinished) {
             Scanner scanner = new Scanner(System.in);
             int selection = scanner.nextInt();
-
+if (wuerfe == 0) {
+    System.out.println("Game Over.");
+    isFinished = true;
+}
             if (selection == 1) {
                 wuerfe = wuerfe - 1;
                 Random random = new Random();
                 int randomNumber = random.nextInt(1, 7);
-                System.out.println("Du hast " + randomNumber + "Gewürfelt!");
+                System.out.println("Du hast " + randomNumber + " Gewürfelt!");
                 int randomNumberBOT = random.nextInt(1, 7);
-                System.out.println("Der Computer hat " + randomNumberBOT + "Gewürfelt!");
+                System.out.println("Der Computer hat " + randomNumberBOT + " Gewürfelt!");
                 if (randomNumber > randomNumberBOT) {
                     System.out.println("Diese Runde hast du gewonnen!");
                     if (randomNumberBOT > randomNumber) {
