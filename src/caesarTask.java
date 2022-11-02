dbe4596f104abcd7db5d63f7b6051aa314e54866
Eyup Yilmaz <eyupy99@gmail.com>
2022-11-02 16:34:42 +0100

    cverschlüsselung

--- src/caesarTask.java
+++ src/caesarTask.java
@@ -1,5 +1,3 @@
-//Bei der Cäsar-Verschlüsselung werden die Buchstaben um eine fixe Anzahl Stellen verschoben (z.B. 2) a wird zu c, f wird zu h, z wird zu b usw.
-//Implementiere eine Methode encrypt(String):String und eine Methode decrypt(String):String
 public class caesarTask {
 
     public static void main(String[] args) {
