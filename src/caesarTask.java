245da976a734f8b94f8adeafd0d7a0847ce42ec6
Eyup Yilmaz <eyupy99@gmail.com>
2022-11-14 11:59:07 +0100

    encrypt

--- src/caesarTask.java
+++ src/caesarTask.java
@@ -1,17 +1,16 @@
 public class caesarTask {
-
     public static void main(String[] args) {
         String result = encrypt(2, "a");
     }
 
     public static String encrypt(int shift, String stringToEncrypt) {
-        String encryptedString = "encrypt";
+
         char[] inputData = stringToEncrypt.toCharArray();
 
-        for (int i = 0; 1 < inputData.length; i++) {
+        for (int i = 0; i < inputData.length; i++) {
             int letter = inputData[i] + shift;
             System.out.println("newLetter:" + Character.toString(letter));
         }
-        return encryptedString;
+        return stringToEncrypt;
     }
 }
