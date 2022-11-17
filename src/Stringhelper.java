89cd9420fc2b36b1737edf4dc055a0d5790c0533
Eyup Yilmaz <eyupy99@gmail.com>
2022-11-17 08:26:45 +0100

    added StringHelper

---
+++ src/Stringhelper.java
@@ -0,0 +1,10 @@
+public class Stringhelper {
+    public static boolean isPalindrome(String text){
+        return false;
+    }
+
+    public static int countLetters(String text, char search){
+
+        return -1;
+    }
+}
